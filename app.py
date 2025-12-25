b225891312115f6f2486fbaec60ee9968687cb11
Nguyen Vu Minh Quang <163733286+Wan172005@users.noreply.github.com>
2025-12-25 11:25:54 +0700

    Change greeting from 'Hello World' to 'Hello Jenkins'
    Test webhook

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ class HelloHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html; charset=utf-8")
         self.end_headers()
-        self.wfile.write(b"<h1>Hello World</h1>")
+        self.wfile.write(b"<h1>Hello Jenkins</h1>")
 
 if __name__ == "__main__":
     server = HTTPServer(("0.0.0.0", 8000), HelloHandler)
     print("Server dang chay tai http://0.0.0.0:8000")
+
     server.serve_forever()
